0228d1063aeef77f9b3038f4b85b408b74e0f94f
Derek Chen (RD-TW) <derek_chen@trend.com.tw>
2017-08-21 13:43:47 +0800

    find_element_by_link_text does not work for Alian channel

--- hinet.py
+++ hinet.py
@@ -17,9 +17,9 @@ if len(sys.argv) > 1:
   prog = sys.argv[1]
   outFile = prog + '_' + time.strftime('%Y%m%d',time.localtime()) + '.ts'
   if prog == 'bluespower':
-    radioId = u"Classical Taiwan愛樂電台"
+    radioId = "Classical Taiwan"
   elif prog == 'music543':
-    radioId = u"NEWS98新聞網"
+    radioId = "Alian"
   else:
     sys.exit(0)
 else:
@@ -27,35 +27,50 @@ else:
 
 site = 'http://radio-hichannel.cdn.hinet.net'
 logFile = 'radio.log'
+urlInfo = '__url.txt'
 pcapFile = 'HiC.cap';
 profDir = 'd:/temp/HiC'
 urlHiChannel = 'http://hichannel.hinet.net/'
 
-try:
-  os.remove(logFile)
-except OSError:
-  pass
+logging.basicConfig(filename=logFile,level=logging.DEBUG,format="%(asctime)s: %(message)s")
+
 fb = webdriver.firefox.firefox_binary.FirefoxBinary("D:/programs/Firefox36/firefox.exe")
 fp = webdriver.FirefoxProfile(profDir) # fp = webdriver.FirefoxProfile() is OK too.
 browser = webdriver.Firefox(firefox_profile=fp,firefox_binary=fb)
 browser.get(urlHiChannel)
-time.sleep(30) #wait for advertisement to finish
+time.sleep(60) #wait for advertisement to finish
 proc = subprocess.Popen(['tshark', '-i 2', '-f tcp port 80', '-c 300', '-w'+pcapFile])
-browser.find_element_by_link_text(radioId).click()
+browser.find_element_by_xpath("//div/p[contains(text(),'{0}')]/../preceding-sibling::a".format(radioId)).click()
+'''
+# problem not solved here: even change the channel during ad, after ad is finished, the page reloads the default program.
+timeout = 60
+found = 0
+while found == 0 and timeout > 0:
+  try:
+    browser.find_element_by_xpath("//div/p[contains(text(),'{0}')]/../preceding-sibling::a".format(radioId)).click()
+    channel = browser.find_element_by_xpath("//p[@id='playingChannel']")
+    if re.search(radioId,channel.text): found = 1
+  except:
+    pass
+  time.sleep(1)
+  timeout -= 1
+'''
 proc.wait()
-f = open(logFile,'w')
+f = open(urlInfo,'w')
 subprocess.call(['tshark', '-r'+pcapFile, '-2', '-R http.request'], stdout=f)
 f.close()
 browser.quit()
 
 reParam = re.compile('(/live/pool/.+?)[^/]+0.m3u8\?token1=([^&]+)&token2=([^&]+)&expire1=(\d+)&expire2=(\d+)')
 matchParam = None
-with open(logFile,'r') as f:
+with open(urlInfo,'r') as f:
   for line in f:
     matchParam = reParam.search(line)
     if matchParam: break
 
-if matchParam is None: sys.exit(0)
+if matchParam is None:
+  logging.info("Failed to find tokens")
+  sys.exit(0)
 
 url = site + matchParam.group(0)
 path,token1,token2,expire1,expire2 = matchParam.groups()
@@ -70,7 +85,6 @@ headers = { 'Host': site[7:], #skip http://
             'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4'
            }
 
-logging.basicConfig(filename=logFile,level=logging.DEBUG,format="%(asctime)s: %(message)s")
 logging.info("opening URL: " + url)
 
 lastId = 0
@@ -123,3 +137,5 @@ while elapsed < duration:
       time.sleep(1)
 
 f.close()
+
+logging.info("exit.")
